3dbabd5355af5d2ef7259c8734095f2863f2dafc
Yishu See <yishuyishu@gmail.com>
2020-10-30 18:21:46 +0800

    Add rough scanner

--- plox/scanner.py
+++ plox/scanner.py
@@ -5,9 +5,18 @@ from token_type import TokenType
 def scan(source):
     tokens = []
     current = 0
+    line = 0
     while current < len(source):
         c = source[current]
-        if source[current : current + 2] == "//":
+        if c in ["!", "=", "<", ">"]:
+            peek = source[current + 1]
+            if peek == "=":
+                tokens.append(Token(TokenType(c + peek)))
+                current += 2
+            else:
+                tokens.append(Token(TokenType(c)))
+                current += 1
+        elif source[current : current + 2] == "//":
             try:
                 current = source.index("\n", current) + 1
             except ValueError:
@@ -20,15 +29,31 @@ def scan(source):
                 break
             value = source[start:current]
             tokens.append(Token(TokenType.STRING, value))
+            current += 1
+        elif c == "\n":
+            line += 1
+            current += 1
         elif c.isalpha():
             word = ""
-            while source[current].isalnum():
+            while source[current].isalnum() or source[current] == "_":
                 word += source[current]
                 current += 1
             try:
                 tokens.append(Token(TokenType(word)))
             except ValueError:
                 tokens.append(Token(TokenType.IDENTIFIER, word))
+        elif c.isdigit():
+            num_str = ""
+            while source[current].isdigit() or source[current] == ".":
+                num_str += source[current]
+                period_removed = num_str.replace(".", "", 1)
+                if not period_removed.isdigit():
+                    print("something wrong")
+                current += 1
+            tokens.append(Token(TokenType.NUMBER, float(num_str)))
         else:
-            # tokens.append(Token(TokenType(c)))
+            try:
+                tokens.append(Token(TokenType(c)))
+            except ValueError:
+                pass
             current += 1
